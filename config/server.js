24b69b919aa2c3836855c6f6b579cf983f9d9b71
Mosiur Rahman <mosiurrahman595@gmail.com>
2024-09-25 19:00:58 +0600

    Update server.js
    da

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://strapi-backend-azure.vercel.app/'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS', ['yourKey1', 'yourKey2', 'yourKey3']),
